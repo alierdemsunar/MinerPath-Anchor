51c00b1fd7e5d91925f5989f04fd7622f0b3d034
mertdluffy <mertdluffy@hotmail.com>
2022-11-30 22:13:06 +0300

    Initial commit

--- lib/UWB/UWBPorting.cpp
+++ lib/UWB/UWBPorting.cpp
@@ -25,67 +25,67 @@ namespace UWBPorting {
         const SPISettings _fastSPI = SPISettings(ArduinoSPImaximumSpeed, MSBFIRST, SPI_MODE0);
 #endif
         const SPISettings _slowSPI = SPISettings(SPIminimumSpeed, MSBFIRST, SPI_MODE0);
-        const SPISettings* _currentSPI = &_fastSPI;
+        const SPISettings *_currentSPI = &_fastSPI;
 
         void _openSPI(uint8_t slaveSelectPIN) {
-        _spi->beginTransaction(*_currentSPI);
-        digitalWrite(slaveSelectPIN, LOW);
+            _spi->beginTransaction(*_currentSPI);
+            digitalWrite(slaveSelectPIN, LOW);
+        }
+
+        void _closeSPI(uint8_t slaveSelectPIN) {
+            digitalWrite(slaveSelectPIN, HIGH);
+            _spi->endTransaction();
+        }
     }
 
-    void _closeSPI(uint8_t slaveSelectPIN) {
-    digitalWrite(slaveSelectPIN, HIGH);
-    _spi->endTransaction();
-}
-}
+    void SPIinit(SPIClass &spi) {
+        _spi = &spi;
+        _spi->begin();
+    }
 
-void SPIinit(SPIClass &spi) {
-    _spi = &spi;
-    _spi->begin();
-}
+    void SPIend() {
+        _spi->end();
+    }
 
-void SPIend() {
-    _spi->end();
-}
-
-void SPIselect(uint8_t slaveSelectPIN, uint8_t irq) {
+    void SPIselect(uint8_t slaveSelectPIN, uint8_t irq) {
 #if !defined(ESP32) && !defined(ESP8266)
-if(irq != 0xff)
-_spi->usingInterrupt(digitalPinToInterrupt(irq));
+        if (irq != 0xff)
+            _spi->usingInterrupt(digitalPinToInterrupt(irq));
 #endif
-pinMode(slaveSelectPIN, OUTPUT);
-digitalWrite(slaveSelectPIN, HIGH);
-}
-
-void writeToSPI(uint8_t slaveSelectPIN, uint8_t headerLen, byte header[], uint16_t dataLen, byte data[]) {
-_openSPI(slaveSelectPIN);
-for(auto i = 0; i < headerLen; i++) {
-_spi->transfer(header[i]); // send header
-}
-for(auto i = 0; i < dataLen; i++) {
-_spi->transfer(data[i]); // write values
-}
-delayMicroseconds(5);
-_closeSPI(slaveSelectPIN);
-}
-
-void readFromSPI(uint8_t slaveSelectPIN, uint8_t headerLen, byte header[], uint16_t dataLen, byte data[]){
-_openSPI(slaveSelectPIN);
-for(auto i = 0; i < headerLen; i++) {
-_spi->transfer(header[i]); // send header
-}
-for(auto i = 0; i < dataLen; i++) {
-data[i] = _spi->transfer(0x00); // read values
-}
-delayMicroseconds(5);
-_closeSPI(slaveSelectPIN);
-}
-
-void setSPIspeed(SPIClock speed) {
-    if(speed == SPIClock::FAST) {
-        _currentSPI = &_fastSPI;
-    } else if(speed == SPIClock::SLOW) {
-        _currentSPI = &_slowSPI;
+        pinMode(slaveSelectPIN, OUTPUT);
+        digitalWrite(slaveSelectPIN, HIGH);
     }
-}
+
+    void writeToSPI(uint8_t slaveSelectPIN, uint8_t headerLen, byte header[], uint16_t dataLen, byte data[]) {
+        _openSPI(slaveSelectPIN);
+        for (auto i = 0; i < headerLen; i++) {
+            _spi->transfer(header[i]); // send header
+        }
+        for (auto i = 0; i < dataLen; i++) {
+            _spi->transfer(data[i]); // write values
+        }
+        delayMicroseconds(5);
+        _closeSPI(slaveSelectPIN);
+    }
+
+    void readFromSPI(uint8_t slaveSelectPIN, uint8_t headerLen, byte header[], uint16_t dataLen, byte data[]) {
+        _openSPI(slaveSelectPIN);
+        for (auto i = 0; i < headerLen; i++) {
+            _spi->transfer(header[i]); // send header
+        }
+        for (auto i = 0; i < dataLen; i++) {
+            data[i] = _spi->transfer(0x00); // read values
+        }
+        delayMicroseconds(5);
+        _closeSPI(slaveSelectPIN);
+    }
+
+    void setSPIspeed(SPIClock speed) {
+        if (speed == SPIClock::FAST) {
+            _currentSPI = &_fastSPI;
+        } else if (speed == SPIClock::SLOW) {
+            _currentSPI = &_slowSPI;
+        }
+    }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <Arduino.h>
 #include <EthernetUDP.h>
 #include <Terminal.h>
+#include "UWBMain.h"
+
 //***UNIQUE PARAMETERS***//
+#if defined(ESP8266)
+const uint8_t PIN_RST = 5; // reset pin
+const uint8_t PIN_IRQ = 4; // irq pin
+const uint8_t PIN_SS = 15; // spi select pin
+#else
+const uint8_t PIN_RST = 9; // reset pin
+const uint8_t PIN_IRQ = 2; // irq pin
+const uint8_t PIN_SS = SS; // spi select pin
+#endif
+
+
 char TERMINAL_DOMAIN[32] = "terminal.minerpath.local"; // Terminal domain address
 byte ANCHOR_MAC_ADDRESS[6] = {0x90, 0x92, 0xBE, 0xEF, 0xFE, 0xEF}; // Anchor MAC address
 const boolean DEVELOPMENT_MODE = true; // Development mode switch
@@ -12,11 +25,39 @@ unsigned int TERMINAL_PORT = 6792; // Terminal Port
 unsigned int LOCAL_PORT = 6792; // Local Port
 EthernetUDP UDP_PACKET; // EthernetUDP Class
 
+//CONFIGURATIONS
+device_configuration_t DEFAULT_CONFIG = {
+        false,
+        true,
+        true,
+        true,
+        false,
+        SFDMode::STANDARD_SFD,
+        Channel::CHANNEL_5,
+        DataRate::RATE_850KBPS,
+        PulseFrequency::FREQ_16MHZ,
+        PreambleLength::LEN_256,
+        PreambleCode::CODE_3
+};
+
+frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
+        false,
+        false,
+        true,
+        false,
+        false,
+        false,
+        false,
+        false
+};
+
+
 void setup() {
+    Serial.begin(115200);
+
     EthernetClass::begin(ANCHOR_MAC_ADDRESS);
     //***DEVELOPMENT ONLY CODE START***//
     if(DEVELOPMENT_MODE) {
-        Serial.begin(115200);
         while (!Serial) {}
         Serial.println("[?] Trying to activate ethernet module...");
         if (EthernetClass::begin(ANCHOR_MAC_ADDRESS) == 0) {
@@ -36,6 +77,16 @@ void setup() {
     //***DEVELOPMENT ONLY CODE END***//
     TERMINAL_IP = Terminal::terminalDNSLookup(TERMINAL_DOMAIN, TERMINAL_IP, DEVELOPMENT_MODE);
     UDP_PACKET.begin(LOCAL_PORT);
+    // DEBUG monitoring
+    // initialize the driver
+    UWBMain::initialize(PIN_SS, PIN_IRQ, PIN_RST);
+    Serial.println(F("DW1000Ng initialized ..."));
+    // general configuration
+    UWBMain::setDeviceAddress(5);
+    UWBMain::setNetworkId(10);
+    Serial.println(F("Committed configuration ..."));
+    // wait a bit
+    delay(1000);
 }
 
 void loop() {
@@ -43,5 +94,12 @@ void loop() {
     UDP_PACKET.write("Anchor IP: ");UDP_PACKET.write(EthernetClass::localIP());
     UDP_PACKET.endPacket();
     delay(1000);
+    char msg[128];
+    UWBMain::getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    UWBMain::getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    UWBMain::getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
     //Terminal::foo();
 }
